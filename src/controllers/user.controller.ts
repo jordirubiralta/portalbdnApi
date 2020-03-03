6ef76e582a1257ebb04d4447785b7c23c7e71401
Jordi Rubiralta <jordiruby@gmail.com>
2020-03-03 23:05:27 +0100

    add send user data when register

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from 'express'
 import User, {IUser} from '../models/user'
-import jwt from 'jsonwebtoken'
+import jwt, { sign } from 'jsonwebtoken'
 import config from '../config/config'
 function createToken(user: IUser) {
     return jwt.sign({id: user.id, email: user.email}, config.JWTSECRET, {
@@ -15,22 +15,28 @@ export const signUp = async (req: Request, res: Response) => {
     } 
     const user = await User.findOne({email: req.body.email});
     if (user) {
-        return res.sendStatus(400).json({msg: 'L\'usuari ja existeix'});
+        return res.status(400).json({msg: 'L\'usuari ja existeix'});
     }
     const newUser = new User(req.body)
     await newUser.save();
 
-    return res.sendStatus(201).json(newUser);
+    const isMatch = await newUser.comparePassword(req.body.password)
+    if (isMatch) {
+        console.log(newUser.surname)
+        return res.status(200).json({email: newUser.email, name: newUser.name,
+            surname: newUser.surname, age: newUser.age, id: newUser.id, token: 'Bearer ' + createToken(newUser)});
+    }
+    return res.status(201).json(newUser);
 }
 
 export const signIn = async (req: Request, res: Response) => {
     console.log(req.body);
     if (!req.body.email || !req.body.password) {
-        return res.sendStatus(400).json({msg: 'Es necessita email i contrasenya'})
+        return res.status(404).json({msg: 'Es necessita email i contrasenya'})
     }
     const user = await User.findOne({email: req.body.email});
     if (!user) {
-        return res.send(400).json({msg: 'Aquest usuari no està registrat'});
+        return res.status(500).json({msg: 'Aquest usuari no està registrat'});
     }
 
     const isMatch = await user.comparePassword(req.body.password)
@@ -39,5 +45,5 @@ export const signIn = async (req: Request, res: Response) => {
         return res.status(200).json({email: user.email, name: user.name,
             surname: user.surname, age: user.age, id: user.id, token: 'Bearer ' + createToken(user)});
     }
-    return res.send(400).json({msg: 'Contrasenya incorrecte'});
+    return res.status(500).json({msg: 'Contrasenya incorrecte'});
 }
